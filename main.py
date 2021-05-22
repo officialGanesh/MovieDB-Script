5028c974e5bcd8832c6bba38b1e4719d45edade7
Ganesh <ganuofficial2795@gmail.com>
2021-05-22 18:05:05 +0530

    Different company's id

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Import the required module
 
 import csv , json , requests
+from pprint import pprint
 
 """
 
@@ -16,7 +17,7 @@ def MovieDB_script():
 
     api_key = '1ff5e64828c4dd99b310dc81a34c56e0'
     base_url = 'https://api.themoviedb.org/3/'
-    movie_search = 'Avatar'
+    movie_search = 'Marvel'
 
     url = f"{base_url}search/company?api_key={api_key}&query={movie_search}"
 
@@ -24,6 +25,28 @@ def MovieDB_script():
         r = requests.get(url)
         r.raise_for_status
         # print(url)
+
+        def json_file():
+            '''Getting the source and saving into json file'''
+
+            with open('data.json','w') as f:
+                source = r.json()
+                f.write(json.dumps(source,indent=2))
+            
+            # python-dict
+            with open('data.json','r') as f:
+                py_file = json.load(f)
+                # pprint(py_file)
+                # pprint(py_file['results'])
+                results = py_file['results']
+                different_id = []
+                for i in results:
+                    j = i['id']
+                    different_id.append(j)
+                                     
+            
+
+        json_file()
 
     except Exception as e:
         print('Something Went Wrong ',e)
