e53a2a16ed62a4a8d5bc5de15a49fc808747c608
Ganesh <ganuofficial2795@gmail.com>
2021-05-22 17:30:41 +0530

    Initial commit

--- main.py
+++ main.py
@@ -2,11 +2,13 @@
 
 import csv , json , requests
 
+def MovieDB_script():
+    '''Main function to get movie info'''
 
-
-
+    
 
 
 if __name__ == "__main__":
 
+    MovieDB_script()
     print('Code Completed 🔥')
